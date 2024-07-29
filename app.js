549a2552053c694d6dd5d1c4fae6ca3caf40c56c
b_ono <b_ono@eltex.co.jp>
2024-07-30 08:54:43 +0900

    finish

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$(function () {
+  $("#toggleButton").on("click", function () {
+    $("#myParagraph").slideToggle();
+  });
+
+  $("#colorButton").on("click", function () {
+    $("#colorDiv").css("background-color", "yellow");
+  });
+
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").addClass("new-class");
+  });
+
+  $("#removeClassButton").on("click", function () {
+    $("#classDiv").removeClass("new-class");
+  });
+
+  $("#fadeInButton").on("click", function () {
+    $("#fadeDiv").fadeIn();
+  });
+
+  $("#fadeOutButton").on("click", function () {
+    $("#fadeDiv").fadeOut();
+  });
+
+  $("#slideUpButton").on("click", function () {
+    $("#slideDiv").slideUp();
+  });
+
+  $("#slideDownButton").on("click", function () {
+    $("#slideDiv").slideDown();
+  });
+});
